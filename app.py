33a293975e67910fd1a23d50d8b5e90560fe7d29
Rutuja Gurav <rutujagurav72@gmail.com>
2022-04-04 14:49:16 +0530

    finally ckeditor is apply in website :)

--- app.py
+++ app.py
@@ -48,36 +48,19 @@ def about():
 def contact():
     return render_template('contact.html')
 
-
-# def getting_text():
-#     if request.method == 'POST':
-#         data = request.form('editorData')
-
-#         return data
-#     else:
-#         return("Empty")
-
-# @app.route('/get_end_predictions', methods=['POST'])
-# def test():
-#     output = request.get_json()
-#     print(output) # This is the output that was stored in the JSON within the browser
-#     print(type(output))#this shows the json converted as a python dictionary
-#     # return output
-
-
 @app.route('/get_end_predictions', methods=['POST'])
 def get_prediction_eos():
     try:
-        data4 = request.form()
+        data4 = request.get_json()
         raw = BeautifulSoup(data4).get_text()
-        print(raw)
+        # print(raw)
         input_text = ' '.join(raw.split())
-        # input_text += ' <mask>'
-        # # top_k=5
-        # # res = main.get_all_predictions(input_text, top_clean=int(top_k))
-        # # print(res)
-        # # print(json.dumps(res))
-        # return app.response_class(response=json.dumps(res), status=200, mimetype='application/json')
+        input_text += ' <mask>'
+        top_k=5
+        res = main.get_all_predictions(input_text, top_clean=int(top_k))
+        # print(res)
+        # print(json.dumps(res))
+        return app.response_class(response=json.dumps(res), status=200, mimetype='application/json')
     except Exception as error:
         err = str(error)
         print(err)
